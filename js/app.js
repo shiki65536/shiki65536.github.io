d6001734a2319d654ec15836c979cba3364ca42a
爵太郎 <magic@shakutarous-iMac.local>
2022-03-10 06:41:16 +0800

    initial

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector("#nav");
+const toggleBtn = document.querySelector("#toggle-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
